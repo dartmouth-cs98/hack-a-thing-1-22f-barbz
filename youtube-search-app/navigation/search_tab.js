73923d53744c5cc65f16d99a7ca05e0dd9fe3aa7
willperez23 <william.perez.23@dartmouth.edu>
2022-09-19 10:40:17 -0400

    part one done

--- youtube-search-app/navigation/search_tab.js
+++ youtube-search-app/navigation/search_tab.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Button } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-// import VideoList from '../components/video_list';
-// import VideoDetail from '../components/video_detail';
+import VideoList from '../components/video_list';
+import VideoDetail from '../components/video_detail';
 
-const TempSearch = (props) => (<Button onPress={() => { props.navigation.navigate('Detail'); }} title="next" />);
-const TempDetail = (props) => (<Button onPress={() => { props.navigation.pop(); }} title="close" />);
+// const TempSearch = (props) => (<Button onPress={() => { props.navigation.navigate('Detail'); }} title="next" />);
+// const TempDetail = (props) => (<Button onPress={() => { props.navigation.pop(); }} title="close" />);
 
 const Stack = createStackNavigator();
 
@@ -14,9 +14,14 @@ const Stack = createStackNavigator();
 // "name" prop is the name of the route
 const SearchTab = () => {
   return (
+    
     <Stack.Navigator>
-      <Stack.Screen name="Search" component={TempSearch} />
-      <Stack.Screen name="Detail" component={TempDetail} />
+      <Stack.Screen name="Search" component={VideoList} options={{
+                                                        title: 'Youtube Search', 
+                                                        headerStyle: { backgroundColor: '#f4511e',},
+                                                        headerTintColor: '#fff',}} 
+                                                        />
+      <Stack.Screen name="Detail" component={VideoDetail} />
     </Stack.Navigator>
   );
 }
